c5ceb0b78227e2fe05b5901a10761b90e7c33d47
Twofold-One <evan_geras@mail.ru>
2021-08-14 00:32:37 +0700

    roundResult function is ready

--- scripts/main.js
+++ scripts/main.js
@@ -14,11 +14,11 @@ function computerPlay() {
     // function assigns integer 1, 2, 3 to Rock, Paper, Scissors accordingly
     function assignFunc() {
         if (randomVar === 1) {
-            return randomVar = "Rock";
+            return randomVar = "rock";
         } else if (randomVar === 2) {
-            return randomVar = "Paper"; 
+            return randomVar = "paper"; 
         } else if (randomVar === 3) {
-            return randomVar = "Scissors";
+            return randomVar = "scissors";
         } else {
             alert("something gone wrong!")
         }
@@ -29,4 +29,143 @@ function computerPlay() {
 
 }
 
-console.log(computerPlay());
+//function that plays single round
+function playRound (playerSelection, computerSelection) {
+    
+    //round result variable
+    let playRoundResult = "default";
+    
+    //huge construction without separate switch functions
+    
+    /*
+    //playRound condition
+    if (playerSelection === "rock") {
+        switch (computerSelection) {
+            case "rock":
+                playRoundResult = "Tie";
+                console.log("Tie, play again");
+                break;
+            case "paper":
+                playRoundResult = "Lose";
+                console.log("You lose! Paper beats Rock");
+                break;
+            case "scissors":
+                playRoundResult = "Win";
+                console.log("You win! Rock beats Scissors");
+                break;
+        }
+        return playRoundResult;         
+    } else if (playerSelection === "paper") {
+        switch (computerSelection) {
+            case "rock":
+                playRoundResult = "Win";
+                console.log("You win! Paper beats Rock");
+                break;
+            case "paper":
+                playRoundResult = "Tie";
+                console.log("Tie, play again");
+                break;
+            case "scissors":
+                playRoundResult = "Lose";
+                console.log("You lose! Scissors beats Paper");
+                break;
+        }
+        return playRoundResult;
+    } else if (playerSelection === "scissors") {
+        switch (computerSelection) {
+            case "Rock":
+                playRoundResult = "Lose";
+                console.log("You lose! Rock beats Scissors");
+                break;
+            case "Paper":
+                playRoundResult = "Win";
+                console.log("You win! Paper beats Rock");
+                break;
+            case "Scissors":
+                playRoundResult = "Tie";
+                console.log("Tie, play again");
+                break;
+        }
+        return playRoundResult;
+    } else {
+        return alert("Error, type a proper object!")
+    }
+*/
+
+    if (playerSelection === "rock") {
+        funcRock();
+        return playRoundResult;         
+    } else if (playerSelection === "paper") {
+        funcPaper();
+        return playRoundResult;
+    } else if (playerSelection === "scissors") {
+        funcScissors();
+        return playRoundResult;
+    } else {
+        alert("Error, type a proper object!");
+        playRoundResult = "error";
+        return playRoundResult;
+    }
+
+    //function if choose Rock
+    function funcRock () {
+        switch (computerSelection) {
+            case "rock":
+                playRoundResult = "Tie";
+                console.log("Tie, play again");
+                break;
+            case "paper":
+                playRoundResult = "Lose";
+                console.log("You lose! Paper beats Rock");
+                break;
+            case "scissors":
+                playRoundResult = "Win";
+                console.log("You win! Rock beats Scissors");
+                break;
+        }
+    }
+
+    //function if choose Paper
+    function funcPaper () {
+        switch (computerSelection) {
+            case "rock":
+                playRoundResult = "Win";
+                console.log("You win! Paper beats Rock");
+                break;
+            case "paper":
+                playRoundResult = "Tie";
+                console.log("Tie, play again");
+                break;
+            case "scissors":
+                playRoundResult = "Lose";
+                console.log("You lose! Scissors beats Paper");
+                break;
+        }
+    }
+    //function if choose Scissors
+    function funcScissors () {
+        switch (computerSelection) {
+            case "rock":
+                playRoundResult = "Lose";
+                console.log("You lose! Rock beats Scissors");
+                break;
+            case "paper":
+                playRoundResult = "Win";
+                console.log("You win! Paper beats Rock");
+                break;
+            case "scissors":
+                playRoundResult = "Tie";
+                console.log("Tie, play again");
+                break;
+        }
+    }
+   
+    
+}
+
+let playerSelection = prompt("Type Rock, Paper or Scissors");
+console.log("Player selection is: " + playerSelection);
+let computerSelection = computerPlay();
+console.log("Computer selection is: " + computerSelection);
+
+console.log("Round result is: " + playRound(playerSelection, computerSelection));
