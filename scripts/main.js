edd995aba3eefb7177ce9722c647c49817434d10
Twofold-One <evan_geras@mail.ru>
2021-08-14 23:25:18 +0700

    Console based version is tested and finished

--- scripts/main.js
+++ scripts/main.js
@@ -35,8 +35,7 @@ function playRound (playerSelection, computerSelection) {
     //round result variable
     let playRoundResult = "default";
     
-    //huge construction without separate switch functions
-    
+    //LEGACY huge construction without separate switch functions
     /*
     //playRound condition
     if (playerSelection === "rock") {
@@ -111,15 +110,15 @@ function playRound (playerSelection, computerSelection) {
     function funcRock () {
         switch (computerSelection) {
             case "rock":
-                playRoundResult = "Tie";
+                playRoundResult = "tie";
                 console.log("Tie, play again");
                 break;
             case "paper":
-                playRoundResult = "Lose";
+                playRoundResult = "lose";
                 console.log("You lose! Paper beats Rock");
                 break;
             case "scissors":
-                playRoundResult = "Win";
+                playRoundResult = "win";
                 console.log("You win! Rock beats Scissors");
                 break;
         }
@@ -129,15 +128,15 @@ function playRound (playerSelection, computerSelection) {
     function funcPaper () {
         switch (computerSelection) {
             case "rock":
-                playRoundResult = "Win";
+                playRoundResult = "win";
                 console.log("You win! Paper beats Rock");
                 break;
             case "paper":
-                playRoundResult = "Tie";
+                playRoundResult = "tie";
                 console.log("Tie, play again");
                 break;
             case "scissors":
-                playRoundResult = "Lose";
+                playRoundResult = "lose";
                 console.log("You lose! Scissors beats Paper");
                 break;
         }
@@ -146,15 +145,15 @@ function playRound (playerSelection, computerSelection) {
     function funcScissors () {
         switch (computerSelection) {
             case "rock":
-                playRoundResult = "Lose";
+                playRoundResult = "lose";
                 console.log("You lose! Rock beats Scissors");
                 break;
             case "paper":
-                playRoundResult = "Win";
+                playRoundResult = "win";
                 console.log("You win! Paper beats Rock");
                 break;
             case "scissors":
-                playRoundResult = "Tie";
+                playRoundResult = "tie";
                 console.log("Tie, play again");
                 break;
         }
@@ -163,9 +162,105 @@ function playRound (playerSelection, computerSelection) {
     
 }
 
+//function of 5 score game
+function game() {
+    
+    let playerScore = 0;
+    let computerScore = 0;
+    let roundNum = 0;
+    let roundResult;
+
+    console.log(`
+    Hello there!
+    Pls type Rock, Paper or Scissors in the pop up 
+    window and try to beat the computer.
+    Game is played for total score of 5. 
+    `)
+
+    while (playerScore + computerScore < 5) {
+        
+        //round num iterator
+        ++roundNum;
+
+        let playerSelection = (prompt("Type Rock, Paper or Scissors")).toLowerCase();
+        console.log("Round " + roundNum + " Player selection is: " + playerSelection);
+        let computerSelection = computerPlay();
+        console.log("Round " + roundNum + " Computer selection is: " + computerSelection);
+        console.log("Round " + roundNum +  " result is: ");
+        roundResult = playRound(playerSelection, computerSelection);
+
+        if (roundResult === "win") {
+            ++playerScore;
+        } else if (roundResult === "lose") {
+            ++computerScore;
+        } else if (roundResult === "tie") {
+            //console.log("Tie, play additional round");
+        } else {
+            console.log("Choose proper thing!");
+        }
+
+        //LEGACY
+        /*
+        console.log("Round " + ++roundNum +  " result is: ");
+        playRound(playerSelection, computerSelection);
+        */
+        //console.log("Round " + ++roundNum +  " result is: " + playRound(playerSelection, computerSelection));
+        
+        /*
+        if (playRound(playerSelection, computerSelection) === "win") {
+            playerScore++;
+        } else if (playRound(playerSelection, computerSelection) === "lose") {
+            computerScore++;
+        } else {
+            console.log("Tie, play additional round")
+        }
+        */
+
+        console.log ("Scores: " + "Player " + playerScore + ", "  + "Computer " +  computerScore);
+        
+        //console spacer
+        console.log(`
+        `)
+    }
+
+    
+    if (playerScore > computerScore) {
+        console.log(`
+        Player score is  ${playerScore}
+        Computer score is ${computerScore}
+        Total rounds played ${roundNum}  
+        Player WINS!
+        `);
+        //console.log("Player score is " + playerScore + ", computer score is " + computerScore + ". Player WINS!");
+    } else {
+        console.log(`
+        Player score is  ${playerScore}
+        Computer score is ${computerScore}
+        Total rounds played ${roundNum}  
+        Computer WINS!
+        `);
+        //console.log("Player score is " + playerScore + ", computer score is " + computerScore + ". Computer WINS!");
+    }
+}
+
+game();
+
+
+//LEGACY
+/*
 let playerSelection = prompt("Type Rock, Paper or Scissors");
 console.log("Player selection is: " + playerSelection);
 let computerSelection = computerPlay();
 console.log("Computer selection is: " + computerSelection);
 
 console.log("Round result is: " + playRound(playerSelection, computerSelection));
+*/
+
+/*
+let playerSelection = prompt("Type Rock, Paper or Scissors");
+        console.log("Player selection is: " + playerSelection);
+        let computerSelection = computerPlay();
+        console.log("Computer selection is: " + computerSelection);
+        playRound(playerSelection, computerSelection);
+        console.log("Round " + (playerScore + computerScore + 1) +  " result is: " + playRound(playerSelection, computerSelection));
+        */
